460526303119ce40cc3c4fa3685a8870635d1767
Undying Wraith <86947902+undyingwraith@users.noreply.github.com>
2022-07-22 07:13:50 +0200

    Add try-catch

--- IpfsFileSystem.ts
+++ IpfsFileSystem.ts
@@ -14,31 +14,33 @@ export class IpfsFileSystem {
       create: true,
       parents: true,
     })
-      .then(callback)
+      .then(() => callback(undefined))
       .catch(e => callback(e));
   }
 
   unlink(filename: string, callback: (err: any) => void) {
     this.node.files.rm(this.normalizePath(filename))
-      .then(callback)
+      .then(() => callback(undefined))
       .catch(e => callback(e));
   }
 
   async readFile(filename: string, callback: (err: any, contents: any) => void) {
-    const chunks = [];
+    try {
+      const chunks = [];
 
-    for await (const chunk of this.node.files.read(this.normalizePath(filename))) {
-      chunks.push(chunk);
+      for await (const chunk of this.node.files.read(this.normalizePath(filename))) {
+        chunks.push(chunk);
+      }
+
+      callback(undefined, uint8ArrayConcat(chunks));
+    } catch(e) {
+      callback(e, undefined);
     }
-
-    callback(undefined, uint8ArrayConcat(chunks));
   }
 
   stat(path: string, callback: (err: any, stats: any) => void) {
     this.node.files.stat(this.normalizePath(path))
-      .then(r => {
-        callback(undefined, new IpfsStat(r));
-      })
+      .then(r => callback(undefined, new IpfsStat(r)))
       .catch(e => callback(e, undefined));
   }
 
@@ -47,11 +49,15 @@ export class IpfsFileSystem {
   }
 
   async readdir(path: string, callback: (err: any, contents: any) => void) {
-    const files = [];
-    for await (const file of this.node.files.ls(this.normalizePath(path))) {
-      files.push(file);
+    try {
+      const files = [];
+      for await (const file of this.node.files.ls(this.normalizePath(path))) {
+        files.push(file);
+      }
+      callback(undefined, files.map(f => f.name));
+    } catch(ex) {
+      callback(ex, undefined);
     }
-    callback(undefined, files.map(f => f.name));
   }
 
   rename(fromPath: string, toPath: string, callback: (err: any) => void) {
